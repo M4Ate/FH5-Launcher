c298f441d8861cd538aa6dc30a28e1262b1768e8
M4Ate <mm4ate@gmail.com>
2022-04-29 21:45:18 +0200

    Update launcher.py

--- launcher.py
+++ launcher.py
@@ -1,3 +1,6 @@
+forzaPath = "G:\XBOX_GAMES\Forza Horizon 5\Content\ForzaHorizon5.exe"                                                                   #   The path to your Forza executable(right click on the.exe and click copy as path)
+
+
 import os, psutil
 
 def processRunning(processName):                                                                                                        #   determines wheather a process is running or not
@@ -10,7 +13,7 @@ def processRunning(processName):                                                
     return False
 
 
-forzaPath = "G:\XBOX_GAMES\Forza Horizon 5\Content\ForzaHorizon5.exe"                                                                   #   The path to your Forza executable(right click on the.exe and click copy as path)
+
 os.system("""for /F "skip=3 tokens=3*" %G in ('netsh interface show interface') do (netsh interface set interface %H disable)""")       #   Disable all network adapters
 os.system(""" "{}" """.format (forzaPath))                                                                                              #   Start Forza
 while(not processRunning("forzahorizon5.exe")):                                                                                         #   Wait untill Forza starts
